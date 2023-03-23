ea8ac7516e60a2d0bead3fe770e31ede213a5569
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-03-23 17:00:51 +0000

    Role update

--- resources/views/components/structure/tab.blade.php
+++ resources/views/components/structure/tab.blade.php
@@ -2,7 +2,7 @@
 
 @push($attributes['parent']->tabstack)
     <li class="nav-item">
-        <a class="nav-link @if($attributes['element-index'] == 0) active @endif" id="{{ $attributes['name'] }}-tab" data-toggle="tab" href="#{{ $attributes['name'] }}-pane" role="{{ $attributes['name'] }}" aria-controls="{{ $attributes['name'] }}-pane" aria-selected="false">{{ $attributes['label'] }}</a>
+        <a class="nav-link @if($attributes['element-index'] == 0) active @endif" id="{{ $attributes['name'] }}-tab" data-toggle="tab" href="#{{ $attributes['name'] }}-pane" role="tab" aria-controls="{{ $attributes['name'] }}-pane" aria-selected="false">{{ $attributes['label'] }}</a>
     </li>
 @endpush
 
