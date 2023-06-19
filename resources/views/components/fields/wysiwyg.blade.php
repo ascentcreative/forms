fe83592b74176f92b7ed80885c55b6eaa9e0f535
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-06-19 13:10:36 +0100

    Remove outline offset

--- resources/views/components/fields/wysiwyg.blade.php
+++ resources/views/components/fields/wysiwyg.blade.php
@@ -12,7 +12,7 @@
 
    <div class="wysiwyg-editor @if($styled) wysiwyg-editor-styled @endif" data-alwayson="{{ $alwayson }}" data-toolbar="{{ $toolbar }}" data-palette="{{ join(",", \AscentCreative\CMS\Models\Swatch::all()->transform(function($item, $key) { return str_replace('#', '', $item->hex); })->toArray()) }}" data-unid="{{ $unid }}" style="width: 100%; height: 100%;">
 
-       <div @if(!$readonly) @else class="p-2" @endif id="edit-{{ $unid }}" style="height: 100%; outline-offset: 6px" placeholder="{{ $placeholder }}" >{!! $value !!}</div>
+       <div @if(!$readonly) @else class="p-2" @endif id="edit-{{ $unid }}" style="height: 100%; xoutline-offset: 6px" placeholder="{{ $placeholder }}" >{!! $value !!}</div>
 
     </div>
 
