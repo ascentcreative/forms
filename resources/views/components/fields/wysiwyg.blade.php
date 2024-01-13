160d9d4a2632f9204e63e944ec1863c9c720d729
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-01-13 17:33:14 +0000

    Read only wysiwyg

--- resources/views/components/fields/wysiwyg.blade.php
+++ resources/views/components/fields/wysiwyg.blade.php
@@ -10,18 +10,29 @@
 @section('element')
 
 
-   <div class="wysiwyg-editor @if($styled) wysiwyg-editor-styled @endif" data-alwayson="{{ $alwayson }}" data-toolbar="{{ $toolbar }}" data-palette="{{ join(",", \AscentCreative\CMS\Models\Swatch::all()->transform(function($item, $key) { return str_replace('#', '', $item->hex); })->toArray()) }}" data-unid="{{ $unid }}" style="width: 100%; height: 100%;">
+    @if($readonly)
 
-       <div @if(!$readonly) @else class="p-2" @endif id="edit-{{ $unid }}" style="height: 100%; xoutline-offset: 6px" placeholder="{{ $placeholder }}" >{!! $value !!}</div>
+        <div class="col-form-label border-bottom p-2" style="min-height: 2.5em;">
+         {!! nl2br($value) !!}
+        </div>
 
-    </div>
+    @else
 
-   <div style="display: none">
-        output:
-        <textarea name="{{$name}}" id="output-{{$unid}}" wire:model="{{ dotname($name) }}" class="wysiwyg-output">{!! $value !!}</textarea>
-   </div>
+
+        <div class="wysiwyg-editor @if($styled) wysiwyg-editor-styled @endif" data-alwayson="{{ $alwayson }}" data-toolbar="{{ $toolbar }}" data-palette="{{ join(",", \AscentCreative\CMS\Models\Swatch::all()->transform(function($item, $key) { return str_replace('#', '', $item->hex); })->toArray()) }}" data-unid="{{ $unid }}" style="width: 100%; height: 100%;">
+
+            <div @if(!$readonly) @else class="p-2" @endif id="edit-{{ $unid }}" style="height: 100%; xoutline-offset: 6px" placeholder="{{ $placeholder }}" >{!! $value !!}</div>
+
+        </div>
+
+        <div style="display: none">
+                <textarea name="{{$name}}" id="output-{{$unid}}" wire:model="{{ dotname($name) }}" class="wysiwyg-output">{!! $value !!}</textarea>
+        </div>
+
+   @endif
 
 @overwrite
+
 
 @once
     @push('lib')
