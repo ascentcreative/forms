bbd3811a069d2d0346dcd645d1bb45805df32cd9
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-01-26 11:16:33 +0000

    readonly

--- resources/views/components/fields/textarea.blade.php
+++ resources/views/components/fields/textarea.blade.php
@@ -5,6 +5,16 @@
 
 @section('element')
 
-    <textarea name="{{$name}}" id="{{ nameToId($name) }}" @isset($rows) rows="{{ $rows }}" @endisset @isset($cols) cols="{{ $cols }}" @endisset class="form-control">{{$value}}</textarea>
+    @if($attributes['readonly'])
+
+        <div class="col-form-label border-bottom p-2" style="min-height: 2.5em;">
+            {{ $value }}
+        </div>
+
+    @else
+
+        <textarea name="{{$name}}" id="{{ nameToId($name) }}" @isset($rows) rows="{{ $rows }}" @endisset @isset($cols) cols="{{ $cols }}" @endisset class="form-control">{{$value}}</textarea>
+
+    @endif
 
 @overwrite
