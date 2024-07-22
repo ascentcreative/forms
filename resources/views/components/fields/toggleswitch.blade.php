3c0dc8502554b156fbeeb48c854e09fe559f696f
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-07-22 14:48:15 +0100

    field ID Format

--- resources/views/components/fields/toggleswitch.blade.php
+++ resources/views/components/fields/toggleswitch.blade.php
@@ -7,7 +7,7 @@
 
 <div class="toggle-switch {{ $attributes['class'] ?? '' }}" id="toggle-switch-{{ $name }}">
     @foreach($options as $key=>$disp)
-        <label for="{{$name }}_{{ $key }}" tabindex="0"><input type="radio" id="{{ $name }}_{{ $key }}" name="{{ $name }}" value="{{ $key }}" @if($value==$key) checked @endif/>{{ $disp }}</label>
+        <label for="{{ nameToId($name) }}_{{ $key }}" tabindex="0"><input type="radio" id="{{ nameToId($name) }}_{{ $key }}" name="{{ $name }}" value="{{ $key }}" @if($value==$key) checked @endif/>{{ $disp }}</label>
     @endforeach
     {{-- <label for="{{ $name }}_all" tabindex="0"><input type="radio" id="{{ $name }}_join_all" name="{{ $name }}" value="all" @if($value=="all") checked @endif/>All</label> --}}
 </div>
