5d22de2abc4a48a683c52c1e60c807c0be009f5c
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-08-20 11:04:56 +0100

    Mutator Loading and event unhooking

--- assets/components/ascent-subformloader.js
+++ assets/components/ascent-subformloader.js
@@ -18,9 +18,8 @@ var SubformLoader = {
         // alert('SFLoader: ' + this.element.data('source') );
         let map = this.element.data('map')
 
-
-
         // watch for changes in the watched field, and use that value to request a subform.
+
         $(document).on('change', '[name=' + this.element.data('source') + ']', function (e) {
             // alert('changed');
             $(self.element).css('opacity', 0.5);
@@ -43,9 +42,13 @@ var SubformLoader = {
         });
 
         // load it over AJAX and replace the content accordingly
-
+        $(this.element).addClass("initialised");
       
 
+    },
+
+    _destroy: function() {
+        $(document).off('change', '[name=' + this.element.data('source') + ']');
     }
 
 }
@@ -58,4 +61,28 @@ $.extend($.ascent.SubformLoader, {
 
 $(document).ready(function(){
    $('.subform-loader').subformloader();
+});
+
+
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    // console.log(mutations, observer);
+    // ...
+    $('.subform-loader').not('.initialised').subformloader();
+
+    // if (.removedNodes) console.log(1);
+    // console.log(mutations);
+
+
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+subtree: true,
+childList: true
+//...
 });
--- assets/dist/js/ascent-forms-bundle.js
+++ assets/dist/js/ascent-forms-bundle.js
@@ -1977,12 +1977,32 @@ var SubformLoader = {
         $(self.element).css('opacity', 1);
       });
     }); // load it over AJAX and replace the content accordingly
+
+    $(this.element).addClass("initialised");
+  },
+  _destroy: function _destroy() {
+    $(document).off('change', '[name=' + this.element.data('source') + ']');
   }
 };
 $.widget('ascent.subformloader', SubformLoader);
 $.extend($.ascent.SubformLoader, {});
 $(document).ready(function () {
   $('.subform-loader').subformloader();
+});
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var observer = new MutationObserver(function (mutations, observer) {
+  // fired when a mutation occurs
+  // console.log(mutations, observer);
+  // ...
+  $('.subform-loader').not('.initialised').subformloader(); // if (.removedNodes) console.log(1);
+  // console.log(mutations);
+}); // define what element should be observed by the observer
+// and what types of mutations trigger the callback
+
+observer.observe(document, {
+  subtree: true,
+  childList: true //...
+
 }); // ******
 // Code (c) Kieran Metcalfe / Ascent Creative 2022
 
