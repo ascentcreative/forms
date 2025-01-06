69b981eb799eb0ab9c5eb58fb854f67cab6fca56
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2025-01-06 14:46:17 +0000

    base activation on attributes

--- resources/views/components/fields/support/charlimit.blade.php
+++ resources/views/components/fields/support/charlimit.blade.php
@@ -1,12 +1,11 @@
-
-@if(isset($charlimit)) 
+@if(isset($charlimit) || isset($attributes['charlimit'])) 
 
 @push('scripts')
     <script>
 
         $(document).ready(function() {
             $('[name={{ $name }}]').charlimit({
-                'max': '{{ $charlimit }}',
+                'max': '{{ $charlimit ?? $attributes['charlimit'] }}',
                 'force': true
             });
         });
